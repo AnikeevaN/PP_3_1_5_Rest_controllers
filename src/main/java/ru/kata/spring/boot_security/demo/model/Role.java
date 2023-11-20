c715f8fb2a5d746f85fcd60e5c41c647e22fa80a
Nadezhda <anikeeva.nd@yandex.ru>
2023-11-20 21:27:48 +0400

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,66 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id, String role_user) {
+        this.id = id;
+        this.name = role_user;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
